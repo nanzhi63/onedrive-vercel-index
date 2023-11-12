c5720b5e9d7bcdbba781a2a248fa671796cffae2
南栀 <108507292+nanzhi63@users.noreply.github.com>
2023-11-12 08:44:46 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "擅长捉弄的高木同学",
+  title: "南栀",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
@@ -51,13 +51,9 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'QQ',
-      link: 'https://pd.qq.com/s/cp4k08j75',
-    },
-    {
       name: 'bilibili',
       link: 'https://space.bilibili.com/496009437',
-    },
+    }
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
